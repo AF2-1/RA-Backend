ab731f34da0b2fb9d629505f3d8b7670546ccaea
raiders032 <nameks@naver.com>
2021-12-14 16:57:38 +0900

    init

---
+++ src/main/java/kr/co/tmax/rabackend/RaBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tmax.rabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/tmax/rabackend/RaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tmax.rabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
