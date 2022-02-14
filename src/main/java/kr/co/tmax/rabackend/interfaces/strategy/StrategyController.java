aadd10fa4569bc6d415b522e2527333e6902b15d
Hott-J <jhj13062004@naver.com>
2022-02-14 11:45:18 +0900

    add: transaction 추가

--- src/main/java/kr/co/tmax/rabackend/interfaces/strategy/StrategyController.java
+++ src/main/java/kr/co/tmax/rabackend/interfaces/strategy/StrategyController.java
@@ -6,6 +6,7 @@ import kr.co.tmax.rabackend.domain.simulation.SimulationService;
 import kr.co.tmax.rabackend.interfaces.simulation.SimulationDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -21,6 +22,7 @@ public class StrategyController {
 
     @ApiOperation(value = "시뮬레이션 전략 완료", notes = "시뮬레이션의 전략을 완료 상태로 변경합니다")
     @PostMapping("/simulation/callback")
+    @Transactional
     public void completeStrategy(@RequestParam String simulationId,
                                  @RequestParam String strategyName,
                                  @Validated @RequestBody SimulationDto.CompleteStrategyRequest request,
