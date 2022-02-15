6b5b2f26893377164f805af3c10928273d03bc76
Hott-J <jhj13062004@naver.com>
2022-02-15 12:27:41 +0900

    add: mongo reactive

--- src/main/java/kr/co/tmax/rabackend/domain/simulation/SimulationReader.java
+++ src/main/java/kr/co/tmax/rabackend/domain/simulation/SimulationReader.java
@@ -1,10 +1,14 @@
 package kr.co.tmax.rabackend.domain.simulation;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface SimulationReader {
+    @Transactional
     Optional<Simulation> findById(String simulationId);
+
     List<Simulation>  findByUserId(String userId);
     Optional<Simulation> findByUserIdAndSimulationId(String userId, String simulationId);
 }
