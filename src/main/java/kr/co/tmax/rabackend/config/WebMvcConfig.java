78d2942eaa7fd266e9c94b6003f9d076989f0fd1
DESKTOP-NRU09TN\hakjae_chung <jhj13062004@naver.com>
2022-02-22 16:11:25 +0900

    fix: cors

--- src/main/java/kr/co/tmax/rabackend/config/WebMvcConfig.java
+++ src/main/java/kr/co/tmax/rabackend/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
