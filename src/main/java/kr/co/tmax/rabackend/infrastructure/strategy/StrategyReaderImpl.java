79683c517e7fd4fcddd42474792acdc4ed84805d
Jihun-Hwang <goharrm@ajou.ac.kr>
2022-03-16 11:32:01 +0900

    feat: add stratey infra

---
+++ src/main/java/kr/co/tmax/rabackend/infrastructure/strategy/StrategyReaderImpl.java
@@ -0,0 +1,25 @@
+package kr.co.tmax.rabackend.infrastructure.strategy;
+
+import kr.co.tmax.rabackend.domain.strategy.Strategy;
+import kr.co.tmax.rabackend.domain.strategy.StrategyReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class StrategyReaderImpl implements StrategyReader {
+    private final StrategyRepository strategyRepository;
+
+    @Override
+    public Optional<Strategy> findBySimulationIdAndStrategyName(String simulationId, String strategyName) {
+        return strategyRepository.findBySimulationIdAndName(simulationId, strategyName);
+    }
+
+    @Override
+    public List<Strategy> findAllBySimulationId(String simulationId) {
+        return strategyRepository.findAllBySimulationId(simulationId);
+    }
+}
