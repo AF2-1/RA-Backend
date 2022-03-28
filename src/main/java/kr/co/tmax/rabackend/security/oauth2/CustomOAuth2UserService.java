93266c1cf4e37a76e046060b8b22d02c1e83cac0
DESKTOP-NRU09TN\hakjae_chung <jhj13062004@naver.com>
2022-03-28 16:12:04 +0900

    [Fix] MySQL port change

--- src/main/java/kr/co/tmax/rabackend/security/oauth2/CustomOAuth2UserService.java
+++ src/main/java/kr/co/tmax/rabackend/security/oauth2/CustomOAuth2UserService.java
@@ -25,28 +25,29 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
 
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+        @Override
+        public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+            OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
 
-        try {
-            return processOAuth2User(oAuth2UserRequest, oAuth2User);
-        } catch (AuthenticationException ex) {
-            throw ex;
-        } catch (Exception ex) {
-            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
-            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
-        }
-    }
-
-    private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
-        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
-
-        if (!StringUtils.hasText(oAuth2UserInfo.getEmail())) {
-            throw new OAuth2AuthenticationProcessingException("ID not found from OAuth2 provider");
+            try {
+                return processOAuth2User(oAuth2UserRequest, oAuth2User);
+            } catch (AuthenticationException ex) {
+                throw ex;
+            } catch (Exception ex) {
+                // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
+                throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+            }
         }
 
-        Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
+        private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
+            OAuth2UserInfo oAuth2UserInfo =
+                    OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
+
+            if (!StringUtils.hasText(oAuth2UserInfo.getEmail())) {
+                throw new OAuth2AuthenticationProcessingException("ID not found from OAuth2 provider");
+            }
+
+            Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
 
         if (userOptional.isEmpty()) {
             User newUser = registerNewUser(oAuth2UserRequest, oAuth2UserInfo);
