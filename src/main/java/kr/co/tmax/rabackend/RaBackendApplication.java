b1f3781df1e8b5416357fd93ab5d61e31dbb23e1
DESKTOP-NRU09TN\hakjae_chung <jhj13062004@naver.com>
2022-04-20 14:42:45 +0900

    fix: 서버실 이동으로 인한 ip 변경 및 user db: mysql -> mongo 마이그레이션 작업

--- src/main/java/kr/co/tmax/rabackend/RaBackendApplication.java
+++ src/main/java/kr/co/tmax/rabackend/RaBackendApplication.java
@@ -3,6 +3,7 @@ package kr.co.tmax.rabackend;
 import kr.co.tmax.rabackend.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,7 +12,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableConfigurationProperties(AppProperties.class)
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableAsync
 //@EnableJpaRepositories(basePackages = "kr.co.tmax.rabackend.infrastructure.user")
 //@EnableMongoRepositories(basePackages = {"kr.co.tmax.rabackend.infrastructure.simulation",
