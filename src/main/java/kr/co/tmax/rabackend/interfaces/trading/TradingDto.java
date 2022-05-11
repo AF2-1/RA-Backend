9723d10d140ecbb760968952056414d29695d2cc
황지훈 <goharrm@gmail.com>
2022-05-11 17:24:44 +0900

    chore: callback request DTO naming 수정

--- src/main/java/kr/co/tmax/rabackend/interfaces/trading/TradingDto.java
+++ src/main/java/kr/co/tmax/rabackend/interfaces/trading/TradingDto.java
@@ -2,13 +2,13 @@ package kr.co.tmax.rabackend.interfaces.trading;
 
 import lombok.*;
 
-public class TradingDtoWithEngine {
+public class TradingDto {
 
     @Getter
     @Setter
     @ToString
     @NoArgsConstructor
-    public static class RegisterPortfolioResponse {
+    public static class RegisterPortfolioCallbackRequest {
         private String status;
         private String person_id;
         private String portfolio_id;
