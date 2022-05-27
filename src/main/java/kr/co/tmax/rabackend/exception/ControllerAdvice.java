f1834867b68b864bb0fd01d57f5900f1e3ba038c
황지훈 <goharrm@gmail.com>
2022-05-27 18:29:00 +0900

    feat: MissingServletRequestParameterException, HttpMessageNotReadableException, DateTimeException 예외처리 추가

--- src/main/java/kr/co/tmax/rabackend/exception/ControllerAdvice.java
+++ src/main/java/kr/co/tmax/rabackend/exception/ControllerAdvice.java
@@ -8,10 +8,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.DateTimeException;
 import java.util.Locale;
 
 @RequiredArgsConstructor
@@ -57,5 +61,25 @@ public class ControllerAdvice {
     public CommonResponse onExternalException(ExternalException e) {
         log.error(e.getMessage());
         return CommonResponse.withMessage("외부 모듈 에러");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = DateTimeException.class)
+    public CommonResponse onDateTimeParseException(DateTimeException e) {
+        log.error(e.getMessage());
+        return CommonResponse.withMessage("날짜 및 시간 형식이 올바르지 않습니다");
+   }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public CommonResponse onDHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(e.getMessage());
+        return CommonResponse.withMessage("HTTP 메시지 형식이 올바르지 않습니다");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public CommonResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+        return CommonResponse.withMessage(ex.getParameterName() + " 이 존재하지 않습니다");
     }
 }
